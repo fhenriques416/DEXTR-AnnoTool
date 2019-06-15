3a874744e34759d92d6b05e5fe51b8a85950c65d
Karan Sharma <karan.epost@gmail.com>
2019-06-15 18:54:19 +0200

    (1) tried to improve subplot alignment, but fails. abandoned plan. (2) other improvments made.

--- annotate.py
+++ annotate.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 
 import argparse
-from os import path, makedirs, listdir
+from os import path
 
 from PIL import Image
 import numpy as np
 from matplotlib import pyplot as plt
-from matplotlib import gridspec
 
 import tensorflow as tf
 from keras import backend as K
@@ -22,6 +21,7 @@ modelName = 'dextr_pascal-sbd'
 pad = 50
 thres = 0.8
 gpu_id = 0
+
 
 if __name__ == "__main__":
 
@@ -56,9 +56,17 @@ if __name__ == "__main__":
         # -----------------------------------Start annotation interface-------------------------------------------------
         _KEEPannotating = True  # annotation status flag
 
+        # Initialise the UI
+        plt.switch_backend('TkAgg')  # force 'TkAgg' backend
+        ui = plt.figure('Annotation Interface')
+        manager = plt.get_current_fig_manager()
+        manager.resize(*manager.window.maxsize()) # maximize window
+        plt.ion()  # setup interactive mode
+
         for filename in files_to_annotate:
 
             if _KEEPannotating:
+
                 # read image
                 imagepath = path.join(cfg['required']['source'], filename)
                 image = np.array(Image.open(imagepath))
@@ -67,38 +75,35 @@ if __name__ == "__main__":
                 _ANNOinit = False
                 _ANNOwrong = False
 
-                # setup annotation UI
-                if not _ANNOinit:
-                    plt.switch_backend('TkAgg') # force 'TkAgg' backend
-                    ui = plt.figure('Annotation Interface')
-                    ui.tight_layout()
+                # -----Setup figure for annotating -----
+                ui.clear()
 
-                # maximize window and setup interactive mode
-                manager = plt.get_current_fig_manager()
-                manager.resize(*manager.window.maxsize())
-                plt.ion()
-                # setup gridspec
-                gs = gridspec.GridSpec(2, 2, width_ratios=[5,1], height_ratios=[11,1])
+                gs = ui.add_gridspec(2, 2, width_ratios=[5, 1], height_ratios=[11, 1])
                 # setup window for images
                 ui_img = ui.add_subplot(gs[0])
                 ui_img.axis('off')
                 ui_img.imshow(image)
                 # window for annotation instructions
-                ui_instruct = ui.add_subplot(gs[1])
+                ui_instruct = ui.add_subplot(gs[1], sharex=ui_img, sharey=ui_img)
                 ui_instruct.axis('off')
                 instruct_msg = ui_instruct.text(0, 0,
-                                                'Instructions:\n1. Use the left mouse button to select the following'
-                                                ' four extreme points for an object in the image:\n-left,'
-                                                '\n-right,\n-top, and\n-bottom.\n2. In case of misselection, use'
-                                                ' right mouse button to undo.\n3. When the 4 points are selected,'
-                                                ' the resulting masks are overlayed.\n4. Status messages are '
-                                                'displayed at bottom of the image; perform next actions based on'
-                                                'them.', fontsize = 14, wrap = True,
-                                                transform = ui_instruct.transAxes)
-
-                ui_status = ui.add_subplot(gs[2]) # window for displaying status
+                                                'Instructions:\n'
+                                                '1. Use the left mouse button to select the\n'
+                                                'following four extreme points for an object:\n'
+                                                '- left,\n'
+                                                '- right,\n'
+                                                '- top, and\n'
+                                                '- bottom.\n\n'
+                                                '2. In case of misselection, use right mouse\n'
+                                                'button to undo.\n\n'
+                                                '3. When the 4 points are selected, the\n'
+                                                'resulting masks are overlayed.\n\n'
+                                                '4. Status messages are displayed at bottom\n'
+                                                'of the image; provide input based on them.',
+                                                fontsize=12, wrap=True, va='top', ha='left')
+                # window for displaying status
+                ui_status = ui.add_subplot(gs[2], sharey=ui_img)  # window for displaying status
                 ui_status.axis('off')
-
 
                 # hold predictions from image
                 image_results = []
@@ -107,19 +112,21 @@ if __name__ == "__main__":
                 while True:
 
                     if not _ANNOinit:
-                        ui_msg = ui_status.text(0.0, 0.0, 'Start annotation...', color='green', fontsize=14)
+                        ui_msg = ui_status.text(0, 0, 'Start annotation...', color='green', wrap=True, fontsize=14,
+                                                va='top', ha='left')
                         _ANNOinit = True
 
                     elif _ANNOinit:
                         if not _ANNOwrong:
-                            ui_msg = ui_status.text(0.0, 0.0, 'Continue annotation...', color='green',
-                                             fontsize=14)
+                            ui_msg = ui_status.text(0, 0, 'Continue annotation...', color='green', wrap=True,
+                                                    fontsize=14, va='top', ha='left')
                         elif _ANNOwrong:
                             ui_img.imshow(helpers.overlay_masks(image / 255, image_results))
-                            ui_msg = ui_status.text(0.0, 0.0, 'Ok. redo annotation...', color='green',
-                                             fontsize=14)
+                            ui_msg = ui_status.text(0, 0, 'Ok. redo annotation...', color='green', wrap=True,
+                                                    fontsize=14, va='top', ha='left')
                             _ANNOwrong = False
 
+                    # user input required
                     extreme_points_ori = np.array(plt.ginput(4, timeout=0, mouse_add=1)).astype(np.int)
                     ui_msg.remove()
 
@@ -146,8 +153,8 @@ if __name__ == "__main__":
                     # Mask correct or not
                     image_results.append(result)  # add result to list of results even if incorrect for vis purposes
                     plot_mask = ui_img.imshow(helpers.overlay_masks(image / 255, image_results))
-                    ui_msg = ui_status.text(0.0, 0.0, 'Is the generated mask correct? Click mouse for yes,'
-                                                 ' press r to redo', color='orange', fontsize=14)
+                    ui_msg = ui_status.text(0, 0, 'Is the generated mask correct? Click mouse for yes, press r to '
+                                                  'redo', color='orange', wrap=True, fontsize=14, va='top', ha='left')
                     not_anno_correct = plt.waitforbuttonpress()
                     ui_msg.remove()
 
@@ -157,50 +164,53 @@ if __name__ == "__main__":
                         continue
 
                     # Add extreme points to show annotation done for the object
-                    plt.plot(extreme_points_ori[:, 0], extreme_points_ori[:, 1], 'gx')
+                    ui_img.plot(extreme_points_ori[:, 0], extreme_points_ori[:, 1], 'gx')
                     # Check what user wants to do next
-                    ui_msg = ui_status.text(0.0, 0.0,
-                                                 'Next...Press d if done annotating this image or click mouse to'
-                                                 ' continue annotating more objects in this image', color='orange',
-                                      fontsize=14)
+                    ui_msg = ui_status.text(0, 0, 'Next...Press d if done annotating this image or click mouse to '
+                                                  'continue annotating more objects in this image',
+                                            color='orange', wrap=True, fontsize=14, va='top', ha='left')
                     usr_choice = plt.waitforbuttonpress()
                     ui_msg.remove()
 
                     if usr_choice:  # if user presses d then break
-                        ui_msg = ui.text(0.0, 0.0, 'Annotation of this image stopped, mask image saved'
-                                                                   ' and progress updated', color='red', fontsize=14)
+                        ui_msg = ui_status.text(0, 0, 'Annotation of this image stopped, mask image saved and progress'
+                                                      ' updated',
+                                                color='red', wrap=True, fontsize=14, va='top', ha='left')
                         break
 
                 # Show and save the generated masks
                 generated_mask = tools.get_img_segmasks(image, image_results)
                 mask_uint8 = tools.save_mask(generated_mask, cfg, filename)
 
-                # update figure to show subplots
+                # -----Setup figure for showing annotation results -----
                 ui.clear()
-                ax1, ax2 = ui.subplots(1,2)
-                ui.tight_layout()
+                # update figure to show subplots
+                gs_res = ui.add_gridspec(2, 2, height_ratios=[11, 1])
                 # subplot-1: image
-                ax1.axis('off')
-                ax1.imshow(image)
-                ax1.set_title('Original Image')
-                # subplot-2: mask
-                ax2.axis('off')
-                ax2.imshow(mask_uint8, cmap='gray')
-                ax2.set_title('Mask Image\n Note: Mask value scaled to [0 255] for visualisation')
-                # update message at bottom
-                ax1_lowercorner = ax1.get_position().corners()
-                ui_msg= ui.text(ax1_lowercorner[0][0], (ax1_lowercorner[0][1] - 0.02),
-                                'Annotate next image? Click mouse for yes, press e to exit annotation and resume later',
-                                color='orange', fontsize=14)
-                # ask for user response
+                ui_res_img = ui.add_subplot(gs_res[0])
+                ui_res_img.axis('off')
+                ui_res_img.imshow(image)
+                ui_res_img.set_title('Original Image')
+                # subplot-2: mask image
+                ui_res_mask = ui.add_subplot(gs_res[1])
+                ui_res_mask.axis('off')
+                ui_res_mask.imshow(mask_uint8, cmap='gray')
+                ui_res_mask.set_title('Mask Image\n Note: Mask value scaled to [0 255] for visualisation')
+                # subplot-3: status message
+                ui_res_status = ui.add_subplot(gs_res[2], sharey=ui_res_img)  # window for displaying status
+                ui_res_status.axis('off')
+                ui_res_msg = ui_res_status.text(0.0, 0.0, 'Annotate next image? Click mouse for yes, press e to exit'
+                                                          ' annotation and resume later',
+                                                color='orange', wrap=True, fontsize=14, va='top', ha='left')
+
+                # user input required
                 usr_choice = plt.waitforbuttonpress()
                 if usr_choice:  # if user presses e then break
                     _KEEPannotating = False
-                ui_msg.remove()
+                ui_res_msg.remove()
             else:
                 print("Annotation of images stopped. Your progress has been saved. Run script again to resume "
                       "annotation. Exiting...")
                 exit(0)
 
         print("Well Done...Finished annotating all files in the source folder. Exiting...")
-
